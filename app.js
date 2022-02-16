ead737efbe914c3a193eab790f27c42c0132689f
Alex Contreras <alex.code29@gmail.com>
2022-02-16 17:35:06 -0600

    generate the HTML file

--- app.js
+++ app.js
@@ -1,11 +1,14 @@
+const fs = require('fs');
+const generatePage = require('./src/page-template.js');
+
 // takes command-line argument and makes them into arrays starting at index 2 
 // and including the last index
 const profileDataArgs = process.argv.slice(2, process.argv.length);
+const [name, github] = profileDataArgs;
 
 
 
-const printProfileData = profileDataArr => {
-   profileDataArgs.forEach(profileItem => console.log(profileItem));  
-};
-
-printProfileData(profileDataArgs);
+fs.writeFile('index.html', generatePage(name, github), err => {
+   if (err) throw err;
+   console.log('Portfolio complete! Check out index.html to see the output!');
+});
