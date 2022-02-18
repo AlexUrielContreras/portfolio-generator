d0dbf778326ec06fb6f30f31aadbbbf544b6edc4
Alex Contreras <alex.code29@gmail.com>
2022-02-17 19:46:25 -0600

    added inquire and a name question

--- app.js
+++ app.js
@@ -1,14 +1,38 @@
-const fs = require('fs');
-const generatePage = require('./src/page-template.js');
+const inquire = require("inquirer");
+
+inquire
+   .prompt([
+      {
+         type: 'input',
+         name: 'name',
+         message: 'What is your name?'
+      }
+   ])
+   .then(answer => console.log(answer));
+
+
+
+
+
+
+
+
+
+
+
+
+
+//const fs = require('fs');
+//const generatePage = require('./src/page-template.js');
 
 // takes command-line argument and makes them into arrays starting at index 2 
 // and including the last index
-const profileDataArgs = process.argv.slice(2, process.argv.length);
-const [name, github] = profileDataArgs;
+//const [name, github] = profileDataArgs;
 
 
 
-fs.writeFile('index.html', generatePage(name, github), err => {
-   if (err) throw err;
-   console.log('Portfolio complete! Check out index.html to see the output!');
-});
+//fs.writeFile('index.html', generatePage(name, github), err => {
+   //if (err) throw err;
+   //console.log('Portfolio complete! Check out index.html to see the output!');
+//});
+
